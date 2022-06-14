fed2fd9332b0e012fba166619de85328751ef956
Tugakit <tugakit@gmail.com>
2022-06-14 16:10:29 +0200

    Clean implementation of icons and better styling

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,39 +1,41 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import Tweeter from '../../Style/StyledComponents';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { ReactComponent as HomeIcon } from '../../Assets/Accueil.svg';
+import { ReactComponent as CommunityIcon } from '../../Assets/Communautés.svg';
+import { ReactComponent as ExploreIcon } from '../../Assets/Explorer.svg';
+import { ReactComponent as NotificationsIcon } from '../../Assets/Notifications.svg';
+import '../../Assets/Communautés.svg';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <div className="text-2xl font-Regular w-1/5 mb-10 ml-20 mt-4 ">
-      <svg className="w-7 h-7 text-blue fill-current mb-10">
-        <path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z" />
-      </svg>
-      <Link to="/">
-        <div className="IconNav highlight">
-          <svg className="h-10 w-10 " viewBox="0 0 15 30" fill="currentColor">
-            <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
-          </svg>
-          <p className="mb-3 flex ">Home</p>
-        </div>
+    <div className="text-2xl w-1/5 mb-10 ml-20 mt-4 ">
+      <Link to="/" className="flex place-content-left highlight">
+        <HomeIcon fill="" stroke="white" width="26px" class="highlight" />
+        <p className=" Pnav highlight ">Home</p>
       </Link>
-      <Link to="/Explore  ">
-        <p className="mb-3 ml-3 highlight">Explore </p>
+      <Link to="/Explore" className="flex place-content-left highlight">
+        <ExploreIcon fill="white" stroke="" width="26px" class="highlight" />
+        <p className="Pnav highlight">Explore </p>
       </Link>
-      <Link to="/Communities">
-        <p className="mb-3 ml-3 highlight">Communities </p>
+      <Link to="/Communities" className="flex place-content-left highlight">
+        <CommunityIcon fill="white" stroke="" width="26px" />
+        <p className="Pnav highlight">Communities </p>
       </Link>
-      <Link to="/Notifications">
-        <p className="mb-3 ml-3 highlight">Notifications </p>
+      <Link to="/Notifications" className="flex place-content-left highlight">
+        <NotificationsIcon fill="white" stroke="" width="26px" />
+        <p className="Pnav highlight">Notifications </p>
       </Link>
-      <Link to="/Messages">
-        <p className="mb-3 ml-3 highlight">Messages </p>
+      {/* TODO Ajouter une fonction pour invertir les couleurs du Logo quand on est sur Home. On peut faire avec ca: className="invert"*/}
+      {/* TODO  Implementer les dernières images */}
+      <Link to="/Messages" className="flex place-content-left highlight">
+        <p className="Pnav highlight">Messages </p>
       </Link>
-      <Link to="/Profile">
-        <p className="mb-3 ml-3 highlight">Profile </p>
+      <Link to="/Profile" className="flex place-content-left highlight">
+        <p className="Pnav highlight">Profile </p>
       </Link>
-      <Link to="/Settings">
-        <p className="mb-3 ml-3 highlight">Settings </p>
+      <Link to="/Settings" className="flex place-content-left highlight">
+        <p className="Pnav highlight">Plus </p>
       </Link>
       <Tweeter></Tweeter>
     </div>
