5c9d9d955b19da897e5d08b6d5a1ca66e0f9cc66
Tugakit <tuga.kit@gmail.com>
2022-06-17 15:04:58 +0200

    Almost finished header, new outline and Logo

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -7,38 +7,39 @@ import { ReactComponent as NotificationsIcon } from '../../Assets/Notifications.
 import { ReactComponent as MessagesIcon } from '../../Assets/Messages_Header.svg';
 import { ReactComponent as ProfileIcon } from '../../Assets/Profil.svg';
 import { ReactComponent as DotsIcon } from '../../Assets/Plus.svg';
-import '../../Assets/Communautés.svg';
+import { ReactComponent as Logo } from '../../Assets/logo.svg';
 import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <div className="text-2xl w-1/5 mb-10 ml-20 mt-4 ">
-      <Link to="/" className="flex place-content-left highlight">
+    <div className="text-2xl w-1/5 ml-10 text-center place-container-around container ">
+      <Logo fill="white" width="32px" className="mb-20 mr-20"  />
+       {/* TODO Ajouter une fonction pour invertir les couleurs du Logo quand on est sur Home. On peut faire avec ca: className="invert"*/}
+      <Link to="/" className="flex place-content-left pl-10 highlight">
         <HomeIcon fill="" stroke="white" width="26px" class="highlight" />
         <p className=" Pnav highlight ">Home</p>
       </Link>
-      <Link to="/Explore" className="flex place-content-left highlight">
+      <Link to="/Explore" className="flex place-content-left pl-10 highlight">
         <ExploreIcon fill="white" stroke="" width="26px" class="highlight" />
         <p className="Pnav highlight">Explore </p>
       </Link>
-      <Link to="/Communities" className="flex place-content-left highlight">
+      <Link to="/Communities" className="flex place-content-left pl-10 highlight">
         <CommunityIcon fill="white" stroke="" width="26px" />
         <p className="Pnav highlight">Communities </p>
       </Link>
-      <Link to="/Notifications" className="flex place-content-left highlight">
+      <Link to="/Notifications" className="flex place-content-left pl-10 highlight">
         <NotificationsIcon fill="white" stroke="" width="26px" />
         <p className="Pnav highlight">Notifications </p>
       </Link>
-      {/* TODO Ajouter une fonction pour invertir les couleurs du Logo quand on est sur Home. On peut faire avec ca: className="invert"*/}
-      <Link to="/Messages" className="flex place-content-left highlight">
+      <Link to="/Messages" className="flex place-content-left pl-10 highlight">
         <MessagesIcon fill="white" stroke="" width="26px" />
         <p className="Pnav highlight">Messages </p>
       </Link>
-      <Link to="/Profile" className="flex place-content-left highlight">
+      <Link to="/Profile" className="flex place-content-left pl-10 highlight">
         <ProfileIcon fill="white" stroke="" width="26px" />
-        <p className="Pnav highlight">Profile </p>
+        <p className="Pnav highlight">Profile</p>
       </Link>
-      <Link to="/Settings" className="flex place-content-left highlight">
+      <Link to="/Settings" className="flex place-content-left pl-10 highlight">
         <DotsIcon fill="white" stroke="" width="26px" />
         <p className="Pnav highlight">Plus </p>
       </Link>
